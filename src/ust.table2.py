e145177f8686a3ab09e9aeceed708b6249dfbcd8
Alan Lewis <alewis@financepress.com>
2023-03-16 17:02:42 -0700

    adds [1m] [1y] [5y] [all] selectors

---
+++ src/ust.table2.py
@@ -0,0 +1,117 @@
+from dash import Dash, dcc, html, Input, Output
+import os
+import datetime as dt
+import dateutil.relativedelta
+import pandas as pd
+from dash import dash_table as dtab
+from data import rates 
+
+fp = __file__
+fhead, ftail = os.path.split(fp)
+
+CSV_PATH = 'rates.csv'
+
+# load the rates.csv data into a dataframe
+df = rates.load_rates(CSV_PATH)
+
+# get the first and last date in the rates file
+first_date = df.index[0].strftime('%Y-%m-%d')
+last_date_raw = df.index[-1]
+last_date = df.index[-1].strftime('%Y-%m-%d')
+# print(df)
+
+# init_date = '2023-02-01'
+# initial view is last month of data
+init_date_raw = last_date_raw + dateutil.relativedelta.relativedelta(months=-1)
+init_date = init_date_raw.strftime('%Y-%m-%d')
+
+app = Dash(__name__)
+
+def build_df(reqdate):
+    df2 = df.loc[reqdate:last_date]    
+    df3 = pd.DataFrame(df2)
+    df_table_out = df3.reset_index()  # move Dates from index into col 1
+    df_table_out['Date'] = df_table_out['Date'].dt.date # delete times in Date column
+    return df_table_out
+
+# test that build_df works
+df_tmp = build_df(init_date)
+column_list = df_tmp.columns
+selected_df = df_tmp.to_dict('records')  # the dash table component uses this form for its 'data' property
+
+app.layout = html.Div(children=[
+    html.H1(children='Testing Dash: tables'),
+    
+    html.H2(children='file = '+ ftail),       
+    
+    # Table    
+    html.Div([
+        dtab.DataTable(
+            id='data-table',
+            data = selected_df, 
+            columns = [{"name": i, "id": i} for i in column_list],
+            page_size = 10
+            )], style={'width': '49%'}),
+    
+    html.Br(),
+    html.Br(),
+    
+    html.Div(children=[
+        html.Label("Choose your data period"),
+        dcc.RadioItems(
+            ['None','1m', '1y','5y','all'],'None',
+            id = 'radio-items',
+            inline=True
+            )], style={'width': '20%', 'display': 'inline-block'}),
+    
+    html.Div(children=[
+        html.Label('or choose your Starting Date: '),
+        dcc.DatePickerSingle(
+        id='date-picker-single',
+        min_date_allowed=first_date,
+        max_date_allowed=last_date,
+        initial_visible_month=last_date,
+        disabled_days=rates.missing_dates(df),
+        date=init_date
+        )       
+    ])     
+    
+])
+
+@app.callback(
+    Output('data-table', 'data'),  # the table property 'data' is being updated by update_table 
+    Input('radio-items','value'),
+    Input('date-picker-single', 'date')
+)
+    
+def update_table(radio_time_inc, selected_date):
+    if radio_time_inc == 'None':
+        df_table = build_df(selected_date)
+        selected_df = df_table.to_dict('records')
+        return selected_df    
+    if radio_time_inc == '1m':
+        init_date_raw = last_date_raw + dateutil.relativedelta.relativedelta(months=-1)
+        init_date = init_date_raw.strftime('%Y-%m-%d')
+        df_table = build_df(init_date)
+        selected_df = df_table.to_dict('records')
+        return selected_df 
+    if radio_time_inc == '1y':
+        init_date_raw = last_date_raw + dateutil.relativedelta.relativedelta(years=-1)
+        init_date = init_date_raw.strftime('%Y-%m-%d')
+        df_table = build_df(init_date)
+        selected_df = df_table.to_dict('records')
+        return selected_df 
+    if radio_time_inc == '5y':
+        init_date_raw = last_date_raw + dateutil.relativedelta.relativedelta(years=-5)
+        init_date = init_date_raw.strftime('%Y-%m-%d')
+        df_table = build_df(init_date)
+        selected_df = df_table.to_dict('records')
+        return selected_df 
+    if radio_time_inc == 'all':        
+        init_date = first_date
+        df_table = build_df(init_date)
+        selected_df = df_table.to_dict('records')
+        return selected_df 
+    
+if __name__ == '__main__':
+    app.run_server(host='127.0.0.1', debug=True)
